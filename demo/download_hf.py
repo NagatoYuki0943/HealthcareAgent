16afe94aed6868dc17d6e09b0138035a4245cded
yuki <lihongtu12138@outlook.com>
2024-03-25 17:21:14 +0800

    init learn

---
+++ demo/download_hf.py
@@ -0,0 +1,8 @@
+import os
+
+# 设置环境变量
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+# 下载模型
+os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir ./sentence-transformer')
+
